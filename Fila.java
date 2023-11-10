996e6be64214d1f8215bf3f6e302b6baa40a2120
Wayne Eloy <138615880+EloyWayne@users.noreply.github.com>
2023-11-10 13:27:49 -0300

    Teste.Fila

---
+++ Fila.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public  class Fila {
+    public static void main(String[] args) {
+        // int[] numeros = new int[ 10 ];
+        // numeros[0] = 1;
+        // System.out.println(numeros[0]);
+
+        ArrayList<Integer> numeros = new ArrayList<>();
+
+        numeros.add(1);
+        System.out.println(numeros);
+    }
+}
